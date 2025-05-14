6e80a80ddd9403380b663d1b81a93e630e11d1e4
Rhayssa Andretto <rha.andretto@gmail.com>
2025-05-13 21:47:25 -0300

    initial commit

---
+++ atividadeJWT2BIM/src/main/java/com/example/atividadeJWT2BIM/AtividadeJwt2BimApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadeJWT2BIM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeJwt2BimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeJwt2BimApplication.class, args);
+	}
+
+}
---
+++ atividadeJWT2BIM/src/test/java/com/example/atividadeJWT2BIM/AtividadeJwt2BimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadeJWT2BIM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeJwt2BimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
